ebc95f49be99e1e7791d91e76e287b0b29b6c6a4
gyozasushi <denisabizar54@gmail.com>
2025-12-24 21:49:48 +0700

    add element to home : title

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,7 +1,14 @@
 export default function Home(){
     return(
-        <div>
-            anak monyet
-        </div>
+        <section className="p-4  border border-black min-h-screen w-full">
+            <div className="flex items-center border border-black h-30 w-full">
+                <h1 className=" text-4xl border border-black">Selamat Pagi Abizar</h1>
+            </div>
+
+            <div className="border border-black p-2">
+                <h4>Absen berikutnya dimulai pukul 08:00</h4>
+            </div>
+            {/* to do: ask permission for gps,add maps view then collect data location from user */}
+        </section>
     )
 }
